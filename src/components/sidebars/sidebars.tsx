2548b87f6c9d709f6e5ce9f5727e4bbd1fb771af
nakagawa333 <naka33321@gmail.com>
2023-08-28 02:37:29 +0900

    不要処理削除

--- src/components/sidebars/sidebars.tsx
+++ src/components/sidebars/sidebars.tsx
@@ -17,98 +17,92 @@ import { useRouter } from 'next/navigation';
 
 
 type Props = {
-    mobileOpen:boolean;
-    handleDrawerToggle:() => void;
+  mobileOpen: boolean;
+  handleDrawerToggle: () => void;
 }
 
-export default function Sidebars(props:Props){
+export default function Sidebars(props: Props) {
   const router = useRouter();
-  const drawerWidth:number = 240;
+  const drawerWidth: number = 240;
   const [mobileOpen, setMobileOpen] = useState(true);
 
-  router.push("");
-
-  const onClick = () => {
-    alert("hello")
-  }
-
-  const iconInfos:any = {
-      "音楽":{
-        "href":"/music",
-        "icon":<LibraryMusicIcon/>
-      },
-      '動画':{
-        "href":"/movie",
-        "icon":<LiveTvIcon />,
-      },
-      "電子書籍":{
-        "href":"/ebook",
-        "icon":<BookIcon />
-      }
+  const iconInfos: any = {
+    "音楽": {
+      "href": "/music",
+      "icon": <LibraryMusicIcon />
+    },
+    '動画': {
+      "href": "/movie",
+      "icon": <LiveTvIcon />,
+    },
+    "電子書籍": {
+      "href": "/ebook",
+      "icon": <BookIcon />
+    }
   }
 
   const drawer = (
-      <div>
-        <Toolbar
+    <div>
+      <Toolbar
+        sx={{
+          display: { xs: 'block', sm: 'none' },
+        }}
+      >
+        <ClearIcon
           sx={{
-              display: { xs: 'block', sm: 'none' },
-          }}          
-        >
-          <ClearIcon 
-            sx={{
-              marginTop:"10px",
-              float:"right",
-              color:"rgba(0,0,0,0.87)"
-            }}
-            onClick={props.handleDrawerToggle}
-          />
-        </Toolbar>
-        <Divider />
-        <List>
-          {['音楽', '動画', '電子書籍'].map((text, index) => (
-            <ListItem key={text} disablePadding onClick={() => router.push(iconInfos[text]["href"])}>
-              <ListItemButton>
-                <ListItemIcon>
-                  {iconInfos[text]["icon"]}
-                </ListItemIcon>
-                <ListItemText primary={text}/>
-              </ListItemButton>
-            </ListItem>
-          ))}
-        </List>
-        <Divider />
-      </div>
+            marginTop: "10px",
+            float: "right",
+            color: "rgba(0,0,0,0.87)"
+          }}
+          onClick={props.handleDrawerToggle}
+        />
+      </Toolbar>
+      <Divider />
+      <List>
+        {['音楽', '動画', '電子書籍'].map((text, index) => (
+          <ListItem key={text} disablePadding onClick={() => router.push(iconInfos[text]["href"])}>
+            <ListItemButton>
+              <ListItemIcon>
+                {iconInfos[text]["icon"]}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+    </div>
   );
-    
-  return(
-      <>
-          <Drawer
-              variant="temporary"
-              ModalProps={{
-                  keepMounted: true,
-              }}
-              open={props.mobileOpen}
-              sx={{
-                  display: { xs: 'block', sm: 'none' },
-                  '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
-              }}
-              onClose={props.handleDrawerToggle}
-          >
-              {drawer}
-          </Drawer>
 
-          <Drawer
-              variant="permanent"
-              ModalProps={{
-                  keepMounted: false,
-              }}
-              sx={{
-              display: { xs: 'none', sm: 'block' },
-              '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
-              }}
-              >
-              {drawer}
-          </Drawer>
-      </>
+  return (
+    <>
+      <Drawer
+        variant="temporary"
+        ModalProps={{
+          keepMounted: true,
+        }}
+        open={props.mobileOpen}
+        sx={{
+          display: { xs: 'block', sm: 'none' },
+          '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+        }}
+        onClose={props.handleDrawerToggle}
+      >
+        {drawer}
+      </Drawer>
+
+      <Drawer
+        variant="permanent"
+        ModalProps={{
+          keepMounted: false,
+        }}
+        sx={{
+          display: { xs: 'none', sm: 'block' },
+          '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+        }}
+      >
+        {drawer}
+      </Drawer>
+    </>
   )
 }
